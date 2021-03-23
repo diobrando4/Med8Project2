5c0e7440e6878b733ef691c6e695f482c2badf42
TheoLauritsen <36989361+Thevakorn@users.noreply.github.com>
2021-03-23 14:24:41 +0100

    Extra buildings like toilet and hospitals added. PickUp Script added

---
+++ P8GameURP/Assets/Scripts/PickUpObject.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            print("Item picked up");
+            Destroy(gameObject);
+        }
+    }
+
+}
