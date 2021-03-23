48f9aa621c13bd35e68e2a746620c3874a9fe722
TheoLauritsen <36989361+Thevakorn@users.noreply.github.com>
2021-03-23 15:02:55 +0100

    PickUp updated

--- P8GameURP/Assets/Scripts/PickUpObject.cs
+++ P8GameURP/Assets/Scripts/PickUpObject.cs
@@ -6,11 +6,22 @@ public class PickUpObject : MonoBehaviour
 {
     void OnTriggerEnter(Collider collider)
     {
+        Debug.Log("abc!!!" + collider.tag);
         if (collider.gameObject.tag == "Player")
         {
+            Debug.Log("abc!!!" + collider.tag);
             print("Item picked up");
-            Destroy(gameObject);
+            transform.position = new Vector3(Random.Range(-54, 66), 0, Random.Range(22, 192));
         }
     }
-
+    void OnTriggerStay(Collider collider)
+    {
+        Debug.Log("abc!!!" + collider.tag);
+        if (collider.gameObject.tag == "Player")
+        {
+            Debug.Log("abc!!!" + collider.tag);
+            print("Item picked up");
+            transform.position = new Vector3(Random.Range(-54, 66), 0, Random.Range(22, 192));
+        }
+    }
 }
