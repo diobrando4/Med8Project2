9d507f28765e0d6c9cc144023068552b2dc46077
mb <mb@telemed.dk>
2021-05-03 16:19:18 +0200

    park puzzle v1 and player mechanics update

---
+++ P8GameURP/Assets/Scripts/WaterRender.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterRender : MonoBehaviour
+{
+    int waterCounter;
+    public GameObject middleWater;
+    public GameObject topWater;
+    public GameObject waterpump;
+    MeshRenderer bottomRender;
+    MeshRenderer middleRender;
+    MeshRenderer topRender;
+    Outline BR;
+    Outline MR;
+    Outline TR;
+
+    WaterPump wp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bottomRender = GetComponent<MeshRenderer>();
+        BR = GetComponent<Outline>();
+        middleRender = middleWater.GetComponent<MeshRenderer>();
+        MR = middleWater.GetComponent<Outline>();
+        topRender = topWater.GetComponent<MeshRenderer>();
+        TR = topWater.GetComponent<Outline>();
+        wp = waterpump.GetComponent<WaterPump>();
+        bottomRender.enabled = false;
+        middleRender.enabled = false;
+        topRender.enabled = false;
+        BR.enabled = false;
+        MR.enabled = false;
+        TR.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waterCounter > 0)
+        {
+            bottomRender.enabled = true;
+            BR.enabled = true;
+        }
+        if (waterCounter > 1)
+        {
+            middleRender.enabled = true;
+            topRender.enabled = true;
+            MR.enabled = true;
+            TR.enabled = true;
+        }
+        if (wp.ispumping)
+        {
+            bottomRender.enabled = true;
+            middleRender.enabled = true;
+            topRender.enabled = true;
+            BR.enabled = true;
+            MR.enabled = true;
+            TR.enabled = true;
+        }
+        if(waterCounter<1 && !wp.ispumping)
+        {
+            bottomRender.enabled = false;
+            middleRender.enabled = false;
+            topRender.enabled = false;
+            BR.enabled = false;
+            MR.enabled = false;
+            TR.enabled = false;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "basket")
+        {
+            if (other.gameObject.GetComponent<BascketScript>().IHaveWater()) {
+                waterCounter++;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "basket")
+        {
+            waterCounter--;
+        }
+    }
+
+    public bool FountainComplete()
+    {
+        return bottomRender.enabled && middleRender.enabled && topRender.enabled;
+    }
+}
--- P8GameURP/Assets/Scripts/basicmovement.cs
+++ P8GameURP/Assets/Scripts/basicmovement.cs
@@ -8,7 +8,7 @@ public class basicmovement : MonoBehaviour
     private Rigidbody rb;
     [SerializeField] private GameObject GrabPos;
 
-    [Range(0.2f, 5)] public float jumpSpeed = 0.5f;
+    [Range(0.2f, 10)] public float jumpSpeed = 0.5f;
     [Header("Speed")]
     [Tooltip("start Speed is the start value, max speed is x2 of the start speed, crouch speed is start speed/2")]
     [Range(0.5f, 5)] public float speed = 5;
@@ -32,8 +32,8 @@ public class basicmovement : MonoBehaviour
     private float downDisRange;
     private float downDis = 0.1f;
     private float jump = 0;
-    private float vertical=0;
-    private float horizontal=0;
+    private float vertical = 0;
+    private float horizontal = 0;
     private float startSpeed = 5;
     private float maxSpeed = 10;
     private float crouchSpeed = 1f;
@@ -46,25 +46,30 @@ public class basicmovement : MonoBehaviour
     private bool forward;
     private bool backward;
     private bool crouching;
-    [HideInInspector]public bool grabbing;
-    [HideInInspector]public bool playJumpAnimation;
-    private bool canJump=false;
+    private bool flying;
+    private bool startJumpCounter;
+    [HideInInspector] public bool grabbing;
+    [HideInInspector] public bool playJumpAnimation;
+    private bool canJump = false;
+    private bool fall = false;
+    private bool jumpCountTest;
     private bool jumpIsMaxed;
-
+    float velY = 1;
 
     [Header("max increments depended on jumpincrements see tooltip")]
     [Tooltip("if jumpincrements is 0.1 and maxincrements max jump height will be achived after 1 sec")]
     public int maxIncrements = 10;
-    private int increments=0;
+    private int increments = 0;
     private int counter;
-    
-    
-    
+    private int testCoutner = 0;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+
         downDirection = Vector3.down;
         startSpeed = speed;
         maxSpeed = startSpeed * 2;
@@ -91,18 +96,39 @@ public class basicmovement : MonoBehaviour
         move();
         grab();
         jumping(jumpIncrements); Debug.Log("canJump" + canJump);
+
+        //  if (!onSurface() && grabbing&& vertical!=0 && !canJump|| !onSurface() && grabbing && horizontal != 0 && !canJump) { preventFlying(1.5f); };
+        test();
+        preventFlying();
+        if (!flying && !onSurface() && grabbing && vertical != 0 || !flying && !onSurface() && grabbing && horizontal != 0) { };
+        // Debug.LogError(counter % Mathf.Round(1.5f / Time.fixedDeltaTime));
+        //preventFlying(0.0f);
+        Debug.LogError("first part " + (!flying && !onSurface() && grabbing && vertical != 0));
+        // Debug.LogError("test =  "+test());
+        //Debug.LogError("total  =  "+(!flying && !onSurface() && grabbing && vertical != 0 && test()));
+
+        /*if (!flying && !onSurface() && grabbing && vertical != 0 && test() ||test() && !flying && !onSurface() && grabbing && horizontal != 0)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, Physics.gravity.y + rb.velocity.y, rb.velocity.z);
+        }*/
+        if(transform.position.y<0 && !onSurface()){
+            transform.position = new Vector3(transform.position.x, 1f, transform.position.z);
+
+        }
     }
-    
+
     void move()
     {
         Vector3 jumping = Vector3.up * rb.velocity.y * jumpSpeed;
         Vector3 j = Vector3.zero;
         vertical = Input.GetAxis("Vertical") * runSpeed();
-        
+
         horizontal = Input.GetAxis("Horizontal") * runSpeed();
         jump = Input.GetAxis("Jump");
-        if (!rayHitStop()) {
-            if (!rayHit()) {
+        if (!rayHitStop())
+        {
+            if (!rayHit())
+            {
                 // rb.velocity = (transform.right * vertical + transform.forward * horizontal) * runSpeed();
                 //rb.velocity = (transform.forward * horizontal + transform.right * vertical) ;
                 rb.MovePosition(rb.position + (transform.right * vertical) * runSpeed() * Time.fixedDeltaTime);
@@ -111,51 +137,122 @@ public class basicmovement : MonoBehaviour
                 /*Debug.Log("velocity x = "+rb.velocity.x);
                 Debug.Log("velocity y = "+rb.velocity.y);
                 Debug.Log("velocity z = "+rb.velocity.z);*/
-            } else if (rayHit() && backward)
+            }
+            else if (rayHit() && backward)
             {
                 rb.MovePosition(rb.position + (transform.right * vertical) * runSpeed() * Time.fixedDeltaTime);
 
-            }else{
+            }
+            else
+            {
                 speed--;
             }
-            
+
         }
         else { rb.velocity = Vector3.zero; Debug.LogWarning(hit.collider.name); speed--; }
         // jump if player is on a collider
-        
+
+
+
+
+    }
+    bool degrease = false; int count = 0;
+    void preventFlying()
+    {
+
+        if (fall && vertical != 0 || horizontal != 0 && fall)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, Physics.gravity.y + rb.velocity.y, rb.velocity.z);
+        }
 
 
 
     }
 
-    void jumping(float increaseForcePrMiliSec){
-
-        
-        if (jump > 0 && onSurface())
+    void test()
+    {
+        if (startJumpCounter && !onSurface())
         {
-            canJump=true;
-           
+            testCoutner++;
+        }
+       /* if (testCoutner % Mathf.Round(1f / Time.fixedDeltaTime) == 0 && startJumpCounter && !onSurface() && !grabbing)
+        {
+            flying = false;
+            startJumpCounter = false;
+            testCoutner = 0;
+            fall = true;
+
+
+
+        }*/
+        if (jump>0){
+            jumpCountTest = true;
+        }
+        if(jumpCountTest && jump==0){
+            if (testCoutner % Mathf.Round(1.5f / Time.fixedDeltaTime) == 0 && startJumpCounter && !onSurface() && grabbing)
+            {
+                flying = false;
+                startJumpCounter = false;
+                testCoutner = 0;
+                fall = true;
+                jumpCountTest = false;
+            }
+        }else{
+            if (testCoutner % Mathf.Round(1.5f / Time.fixedDeltaTime) == 0 && startJumpCounter && !onSurface() && grabbing&&!jumpCountTest)
+            {
+                flying = false;
+                startJumpCounter = false;
+                testCoutner = 0;
+                fall = true;
+                
+            }
+        }
+        
+        if (fall && onSurface())
+        {
+            fall = false;
         }
 
-        if(canJump){
-            
-            if (counter % Mathf.Round(increaseForcePrMiliSec / Time.fixedDeltaTime) == 0 && jump > 0 && canJump && increments <= maxIncrements)
+    }
+
+
+    void jumping(float increaseForcePrMiliSec)
+    {
+
+
+        if (jump > 0 && onSurface())
+        {
+            canJump = true;
+            flying = true;
+        }
+
+        if (canJump)
+        {
+
+            if (counter % Mathf.Round(increaseForcePrMiliSec / Time.fixedDeltaTime) == 0 && jump > 0 && canJump && increments <= maxIncrements && !Input.GetKey(KeyCode.LeftShift))
             {
 
                 jumpSpeed += jumpForceIncrements;
                 increments++;
-                
+
             }
         }
-        if (jump==0 && canJump){
+        if (jump == 0 && canJump)
+        {
+            if(grabbing){
+                jumpSpeed = jumpSpeed + 1f;
+            }
             playJumpAnimation = true;
             rb.velocity = new Vector3(0, jumpSpeed, 0);
             jumpSpeed = startJumpSpeed;
             increments = 0;
             canJump = false;
             counter = 0;
+            startJumpCounter = true;
+
         }
-        
+
+
 
 
 
@@ -163,27 +260,30 @@ public class basicmovement : MonoBehaviour
 
 
 
-    void grab(){
+    void grab()
+    {
         float y;
-        if (rayHit() && Input.GetKey(KeyCode.E) && hit.collider.attachedRigidbody)
+        if (rayHit() && Input.GetKey(KeyCode.E) && hit.collider.attachedRigidbody && grabbedObject==null)
         {
-            
-            
-            
+
+
+
             grabbedObject = hit.collider.gameObject.GetComponent<Rigidbody>();
 
-            if (grabbedObject.mass < rb.mass+110f) {
-                
+            if (grabbedObject.mass < rb.mass + 110f)
+            {
+
                 hit.collider.gameObject.layer = 2;
                 grabDirection = transform.position - grabbedObject.transform.position;
-                if (onSurface()) {
+                if (onSurface())
+                {
                     grabY = rb.position.y;
                 }
                 rb.position = new Vector3(rb.position.x, grabY, rb.position.z);
                 Debug.LogWarning("Grabbing");
                 grabbing = true;
-                
-                
+
+
                 //  grabbedObject.position= transform.position- grabDirection ;
                 /*weight = grabbedObject.mass;
                 rb.mass = weight;*/
@@ -197,32 +297,34 @@ public class basicmovement : MonoBehaviour
             }
         }
 
-        if (vertical != 0  && grabbing || horizontal != 0 && grabbing)
+        if (vertical != 0 && grabbing || horizontal != 0 && grabbing)
         {
-            rb.velocity = new Vector3(rb.velocity.x,0,rb.velocity.z);
-            grabbedObject.velocity = rb.velocity*-1;
+            jumping(jumpIncrements);
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+            grabbedObject.velocity = rb.velocity * -1;
             y = grabbedObject.position.y;
 
-            grabbedObject.useGravity = true;
+            rb.useGravity = true;
             grabbedObject.transform.position = GrabPos.transform.position;
         }
-        else if ( grabbing)
+        else if (grabbing)
         {
-
+            jumping(jumpIncrements);
             grabbedObject.useGravity = false;
+            rb.useGravity = true;
             grabbedObject.transform.position = GrabPos.transform.position;
             //grabbedObject.velocity = rb.velocity * -1;
-            
-           // grabbedObject.MovePosition(GrabPos.transform.position);
+
+            // grabbedObject.MovePosition(GrabPos.transform.position);
             //grabbedObject.velocity = GrabPos.transform.position;
             Debug.Log("grabbedObject" + grabbedObject.transform.position + " goal position " + GrabPos.transform.position);
         }
 
-        if (grabbedObject!=null && !Input.GetKey(KeyCode.E))
+        if (grabbedObject != null && !Input.GetKey(KeyCode.E))
         {
             grabbedObject.useGravity = true;
             //grabbedObject.transform.parent = null;
-         
+
             grabbing = false;
             grabbedObject.gameObject.layer = 0;
             Debug.LogWarning("Dropped the object");
@@ -232,9 +334,10 @@ public class basicmovement : MonoBehaviour
 
 
     }
-    float grabForce(float mass, float accelation){
+    float grabForce(float mass, float accelation)
+    {
         float a = 1 / mass;
-        float force= mass * a;
+        float force = mass * a;
 
         return force;
     }
@@ -256,7 +359,7 @@ public class basicmovement : MonoBehaviour
         Debug.DrawRay(raypos, Camera.main.transform.forward);
         try
         {
-            return Physics.Raycast(raypos, Camera.main.transform.forward, out hit, 0.1f) ;
+            return Physics.Raycast(raypos, Camera.main.transform.forward, out hit, 0.1f);
         }
         catch (Exception e)
         {
@@ -273,20 +376,29 @@ public class basicmovement : MonoBehaviour
     public bool objectInfront()
     {
         //                      origin point,   direction   maxDis
-        return Physics.Raycast(playerPos, Vector3.forward, 0.1f) ;
+        return Physics.Raycast(playerPos, Vector3.forward, 0.1f);
 
     }
-    private float runSpeed(){
+    private float runSpeed()
+    {
 
-        if (!crouching) {
+        if (!crouching)
+        {
             if (Input.GetKey(KeyCode.LeftShift) && !grabbing)
             {
                 speed++;
-            } else {
+            }
+            else
+            {
                 speed--;
             }
+            if(grabbing && !onSurface()){
+                speed = speed + .5f;
+            }
             speed = Mathf.Clamp(speed, startSpeed, maxSpeed);
-        }else{
+        }
+        else
+        {
             speed = crouchSpeed;
         }
         return speed;
