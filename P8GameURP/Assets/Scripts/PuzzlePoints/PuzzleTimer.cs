f11ead94379a83f16a03a5f1db6633041720f9ef
diobrando4 <70629289+diobrando4@users.noreply.github.com>
2021-05-17 15:30:14 +0200

    unfinished puzzle time script

--- P8GameURP/Assets/Scripts/PuzzlePoints/PuzzleTimer.cs
+++ P8GameURP/Assets/Scripts/PuzzlePoints/PuzzleTimer.cs
@@ -15,7 +15,7 @@ public class PuzzleTimer : MonoBehaviour
     private WriteJasonData writeJason;
 
     public int basketCounter = 0;
-    public bool WaterPumpBool =false;
+    public bool WaterPumpBool = false;
     public int PlayerTimeCounter = 0;
     private int fixedCounter = 0;
     public int selfID;
@@ -23,18 +23,34 @@ public class PuzzleTimer : MonoBehaviour
     public string MyName;
     private string selfIDString = string.Empty;
     
-    public bool MyPuzzle = false;
+    public bool MyPuzzle = false; // if false then the puzzle is completed and the sound should be disabled?
     public bool NextPuzzle = false;
     private bool isInside = false;
     private bool isEmergent = false;
-    private bool writeOnce =false;
+    private bool writeOnce = false;
     private MethodInfo MyPuzzleInfo;
     private MethodInfo NextPuzzleInfo;
 
+    public int ECC_1 = 0;
+    public int ECC_2 = 0;
+    public GameObject eventCapsule1;
+    public GameObject eventCapsule2;
+
     private bool GetValuesOnce = false;
     private int nextID;
+
+    // not finished yet :D
+    AudioSource audioSource;
+    public AudioClip emergentStart;
+    public AudioClip emergentSolution;
+
+    // needs something that says "puzzle is completed: ture or false" so it doesn't repeat after being finished
+    // if (MyPuzzleData.MyPuzzle != emergent){} then the sound should no longer play?
+
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+        
         myTimer = new PuzzleTimer();
         writeJason = writeJsonObj.GetComponent<WriteJasonData>();
         playerPositionList = new List<Vector3>();
